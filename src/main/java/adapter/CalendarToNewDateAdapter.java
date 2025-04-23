688bc7a47955b1267af1b86fb6bf97d18f764711
gromov-ivan <ivan-gromov@outlook.com>
2025-04-23 22:59:02 +0300

    Implement Calendar Adapter using Adapter design pattern

---
+++ src/main/java/adapter/CalendarToNewDateAdapter.java
@@ -0,0 +1,53 @@
+package main.java.adapter;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class CalendarToNewDateAdapter implements NewDateInterface {
+
+    private final Calendar cal;
+
+    public CalendarToNewDateAdapter(int year, int month, int day) {
+        this.cal = new GregorianCalendar(year, month - 1, day);
+    }
+
+    @Override
+    public void setDay(int day) {
+        cal.set(Calendar.DAY_OF_MONTH, day);
+    }
+
+    @Override
+    public void setMonth(int m) {
+        cal.set(Calendar.MONTH, m - 1);
+    }
+
+    @Override
+    public void setYear(int year) {
+        cal.set(Calendar.YEAR, year);
+    }
+
+    @Override
+    public int getDay() {
+        return cal.get(Calendar.DAY_OF_MONTH);
+    }
+
+    @Override
+    public int getMonth() {
+        return cal.get(Calendar.MONTH) + 1;
+    }
+
+    @Override
+    public int getYear() {
+        return cal.get(Calendar.YEAR);
+    }
+
+    @Override
+    public void advanceDays(int days) {
+        cal.add(Calendar.DAY_OF_MONTH, days);
+    }
+
+    @Override
+    public String toString() {
+        return "%04d-%02d-%02d".formatted(getYear(), getMonth(), getDay());
+    }
+}
---
+++ src/main/java/adapter/Main.java
@@ -0,0 +1,20 @@
+package main.java.adapter;
+
+public class Main {
+    public static void main(String[] args) {
+
+        NewDateInterface date = new CalendarToNewDateAdapter(2025, 4, 23);
+        System.out.println("Initial date: " + date);
+
+        date.advanceDays(10);
+        System.out.println("Plus 7 days: " + date);
+
+        date.advanceDays(-2);
+        System.out.println("Minus 2 days: " + date);
+
+        date.setDay(22);
+        date.setMonth(2);
+        date.setYear(2003);
+        System.out.println("Reset to: " + date);
+    }
+}
